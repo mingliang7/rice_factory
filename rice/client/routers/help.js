e37412d2919ec50a71503d1a24d13849f8ef174a
narong <narongsao2015@gmail.com>
2015-10-16 17:41:18 +0700

    update help

--- rice/client/routers/help.js
+++ rice/client/routers/help.js
@@ -1,11 +1,11 @@
 riceRoutes.route('/help', {
-  name: 'rice.help',
-  action: function(params, queryParams) {
-    Layout.main('rice_help');
-  },
-  breadcrumb: {
-    // params: ['customerId', 'saleId'],
-    title: 'help',
-    parent: 'rice.help'
-  }
+    name: 'acc.help',
+    action: function (params, queryParams) {
+        /*Layout.main('acc_help');*/
+        BlazeLayout.render('helpLayout',{ content: 'acc_help'})
+    },
+    breadcrumb: {
+        title: 'Home'
+    }
 });
+
