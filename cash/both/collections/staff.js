696efafd0a34f73a95df1d94a1acabca97826a60
kevin <faithful.7up@gmail.com>
2015-11-18 10:17:46 +0700

    update cash staff

--- cash/both/collections/staff.js
+++ cash/both/collections/staff.js
@@ -11,62 +11,63 @@ Cash.Collection.Staff = new Mongo.Collection("cash_staff");
  * @type {SimpleSchema}
  */
 Cash.Schema.Staff = new SimpleSchema({
-    name: {
-        type: String,
-        label: "Name",
-        unique: true,
-        max: 200
-    },
-    gender: {
-        type: String,
-        label: "Gender",
-        autoform: {
-            type: "select2",
-            options: function () {
-                return Cash.List.gender();
-            }
-        }
-    },
-    dob: {
-        type: String,
-        label: "Date of Birth"
-    },
-    address: {
-        type: String,
-        label: "Address",
-        autoform: {
-            type: "select2",
-            options: function () {
-                return Cash.List.address();
-            }
-        }
-    },
-    telephone: {
-        type: String,
-        label: "Telephone",
-        optional: true
-    },
-    email: {
-        type: String,
-        label: "Email",
-        regEx: SimpleSchema.RegEx.Email,
-        optional: true
-    },
-    photo: {
-        type: String,
-        autoform: {
-            afFieldInput: {
-                type: 'fileUpload',
-                collection: 'Files',
-                accept: 'image/*'
-            }
-        },
-        optional: true
-    },
-    branchId: {
-        type: String,
-        label: "Branch"
+  name: {
+    type: String,
+    label: "Name",
+    unique: true,
+    max: 200
+  },
+  gender: {
+    type: String,
+    label: "Gender",
+    autoform: {
+      type: "select2",
+      options: function () {
+        return Cash.List.gender();
+      }
     }
+  },
+  dob: {
+    type: String,
+    label: "Date of Birth",
+    optional: true
+  },
+  address: {
+    type: String,
+    label: "Address",
+    autoform: {
+      type: "select2",
+      options: function () {
+        return Cash.List.address();
+      }
+    }
+  },
+  telephone: {
+    type: String,
+    label: "Telephone",
+    optional: true
+  },
+  email: {
+    type: String,
+    label: "Email",
+    regEx: SimpleSchema.RegEx.Email,
+    optional: true
+  },
+  photo: {
+    type: String,
+    autoform: {
+      afFieldInput: {
+        type: 'fileUpload',
+        collection: 'Files',
+        accept: 'image/*'
+      }
+    },
+    optional: true
+  },
+  branchId: {
+    type: String,
+    label: "Branch"
+  }
 });
 
 /**
