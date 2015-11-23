38c649a8548773330ce9e21b084f8dfe28822bc4
kevin <faithful.7up@gmail.com>
2015-11-23 11:34:34 +0700

    update staff branch id

--- cash/client/templates/staff/staff.js
+++ cash/client/templates/staff/staff.js
@@ -6,47 +6,47 @@ var indexTpl = Template.cash_staff,
   addressAddonTpl = Template.cash_addressAddon;
 
 // Index
-indexTpl.onCreated(function() {
+indexTpl.onCreated(function () {
   // Create new  alertify
   SEO.set({
     title: 'staff',
     description: 'staff'
   });
-  createNewAlertify(["staff", "addressAddon","staffShow"]);
+  createNewAlertify(["staff", "addressAddon", "staffShow"]);
 });
 
-indexTpl.onRendered(function() {
+indexTpl.onRendered(function () {
   //
 });
 
 indexTpl.helpers({
-  selector: function() {
+  selector: function () {
     var pattern = Session.get('currentBranch');
     //var pattern = new RegExp("^" + branchId.current.branch);
     return {
-      cpanel_branchId: pattern
+      branchId: pattern
     };
   }
 });
 
 indexTpl.events({
-  'click .insert': function(e, t) {
+  'click .insert': function (e, t) {
     alertify.staff(fa("plus", "Staff"), renderTemplate(insertTpl))
       .maximize();
   },
-  'click .update': function(e, t) {
+  'click .update': function (e, t) {
     var data = Cash.Collection.Staff.findOne(this._id);
     alertify.staff(fa("pencil", "Staff"), renderTemplate(updateTpl, data))
       .maximize();
   },
-  'click .remove': function(e, t) {
+  'click .remove': function (e, t) {
     var self = this;
 
     alertify.confirm(
       fa("remove", "Staff"),
       "Are you sure to delete [" + self._id + "]?",
-      function() {
-        Cash.Collection.Staff.remove(self._id, function(error) {
+      function () {
+        Cash.Collection.Staff.remove(self._id, function (error) {
           if (error) {
             alertify.error(error.message);
           } else {
@@ -58,7 +58,7 @@ indexTpl.events({
     );
 
   },
-  'click .show': function(e, t) {
+  'click .show': function (e, t) {
     var data = Cash.Collection.Staff.findOne({
       _id: this._id
     });
@@ -68,36 +68,36 @@ indexTpl.events({
       data.photoUrl = Files.findOne(data.photo).url();
     }
 
-   /* alertify.alert(fa("eye", "Staff"), renderTemplate(showTpl, data));*/
+    /* alertify.alert(fa("eye", "Staff"), renderTemplate(showTpl, data));*/
     alertify.staffShow(fa('eye', 'Staff'), renderTemplate(
-        showTpl,
-        data));
+      showTpl,
+      data));
   }
 });
 
-indexTpl.onDestroyed(function() {
+indexTpl.onDestroyed(function () {
   //
 });
 
 // Insert
-insertTpl.onRendered(function() {
+insertTpl.onRendered(function () {
   datePicker();
 });
 
 insertTpl.events({
-  'click .addressAddon': function(e, t) {
+  'click .addressAddon': function (e, t) {
     alertify.addressAddon(fa("plus", "Address"), renderTemplate(
       addressAddonTpl))
   }
 });
 
 // Update
-updateTpl.onRendered(function() {
+updateTpl.onRendered(function () {
   datePicker();
 });
 
 updateTpl.events({
-  'click .addressAddon': function(e, t) {
+  'click .addressAddon': function (e, t) {
     alertify.addressAddon(fa("plus", "Address"), renderTemplate(
       addressAddonTpl));
   }
@@ -108,50 +108,51 @@ AutoForm.hooks({
   // Customer
   cash_staffInsert: {
     before: {
-      insert: function(doc) {
+      insert: function (doc) {
         var prefix = Session.get('currentBranch') + '-';
         doc._id = idGenerator.genWithPrefix(Cash.Collection.Staff, prefix,
           3);
-        doc.cpanel_branchId = Session.get('currentBranch');
+        doc.branchId = Session.get('currentBranch');
         return doc;
       }
     },
-    onSuccess: function(formType, result) {
+    onSuccess: function (formType, result) {
       alertify.success('Success');
     },
-    onError: function(formType, error) {
+    onError: function (formType, error) {
       alertify.error(error.message);
     }
   },
   cash_staffUpdate: {
-    onSuccess: function(formType, result) {
+    onSuccess: function (formType, result) {
       alertify.staff().close();
       alertify.success('Success');
     },
-    onError: function(formType, error) {
+    onError: function (formType, error) {
       alertify.error(error.message);
     }
   },
   // Address addon
   cash_addressAddon: {
     before: {
-      insert: function(doc) {
+      insert: function (doc) {
         doc._id = idGenerator.gen(Cash.Collection.Address, 3);
         return doc;
       }
-    }/*,
-    onSuccess: function(formType, result) {
-      //alertify.addressAddon();
-      alertify.success('Success Hery');
-    },
-    onError: function(formType, error) {
-      alertify.error(error.message);
-    }*/
+    }
+    /*,
+        onSuccess: function(formType, result) {
+          //alertify.addressAddon();
+          alertify.success('Success Hery');
+        },
+        onError: function(formType, error) {
+          alertify.error(error.message);
+        }*/
   }
 });
 
 // Config date picker
-var datePicker = function() {
+var datePicker = function () {
   var dob = $('[name="dob"]');
   DateTimePicker.date(dob);
 };
